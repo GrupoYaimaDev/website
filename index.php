db09997ca77c7140f64d6be69a78e6802666a428
ElieserSilva <Novaelirs15@gmail.com>
2021-09-08 11:19:27 -0400

    Segundo commit con logo centrado

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<!--?php include './scripts/main.php'; ?-->
+<?php include './scripts/main.php'; ?>
 <html lang="es-VE">
 
 <head>
@@ -7,34 +7,45 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio | GRUPOYAIMA.COM</title>
-    <link rel="stylesheet" href="./css/app.css">
+    <link rel="stylesheet" href="./css/app.css?<?php echo $date ?>">
     <link rel="shortcut icon" href="./src/logo-verde.png" type="image/x-icon">
 </head>
 
 <body class="bg-black" style="background: linear-gradient(357deg, #040404, #313131);">
     <nav class="main full" id="mainNav">
         <div class="max-w-5xl mx-auto flex justify-between">
-            <img src="./src/logo-w.png" id="mainLogo" alt="" style="filter: drop-shadow(-1px 0px 0px orange) saturate(1.3);" class="logo h-16 py-1" srcset="">
             <ul class="mainMenu hidden md:flex">
-                <li><a href="#">Inicio</a></li>
+                <li><a href="#" class="active">Inicio</a></li>
                 <li><a href="#">Productos<i class="flex items-center fi fi-sr-angle-small-down"></i></a></li>
                 <li><a href="#">Nosotros</a></li>
                 <!-- <li><a href="#">Menu<i class="flex items-center fi fi-sr-angle-small-down"></i></a></li> -->
             </ul>
+            <img src="./src/logo-w.png" id="mainLogo" alt="" style="filter: drop-shadow(-1px 0px 0px orange) saturate(1.3);" class="logo h-16 py-1" srcset="">
         </div>
     </nav>
     <header class="main h-screen bg-blue-to_null_invert overflow-hidden cut-bottom">
-        <figure class="bg-img bg-main sliders hidden animated fadeInLeft" id="main-picture" style="background-image: url(./src/slide1.jpg);">
+        <figure class="mask no_top bg-img bg-main sliders hidden animated fadeInLeft" id="main-picture" style="background-image: url(./src/slide1.jpg);">
             <figcaption>Alta-tecnologia he innovacion.</figcaption>
         </figure>
-        <figure class="bg-img bg-main sliders hidden animated fadeInLeft" id="main-picture" style="background-image: url(./src/slide2.jpg);">
+        <figure class="mask no_top bg-img bg-main sliders hidden animated fadeInLeft" id="main-picture" style="background-image: url(./src/slide2.jpg);">
             <figcaption>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</figcaption>
         </figure>
-        <figure class="bg-img bg-main sliders hidden animated fadeInLeft" id="main-picture" style="background-image: url(./src/slide3.jpg);">
+        <figure class="mask no_top bg-img bg-main sliders hidden animated fadeInLeft" id="main-picture" style="background-image: url(./src/slide3.jpg);">
             <figcaption>Materia prima, solo de las mejores siembras del paiz.</figcaption>
         </figure>
-        <button id="buttonSliderPrev" class="text-5xl text-white z-10 left-4 absolute flex items-center hover:text-yellow buttonSlider"> <i class="fi fi-sr-angle-left"></i> </button>
-        <button id="buttonSliderNext" class="text-5xl text-white z-10 right-4 absolute flex items-center hover:text-yellow buttonSlider"> <i class="fi fi-sr-angle-right"></i> </button>
+        <div class="absolute left-0 top-0 w-full h-screen bg-red z-10 flex flex-col justify-center items-center">
+            <picture class="logo-second bg-white bg-opacity-30">
+                <img src="./src/logo-verde.png" alt="" srcset="">
+            </picture>
+            <p class="text-main mt-4 pt-8 text-black text-center tracking-wider uppercase">
+            <span class="popBold text-3xl">GRUPO YAIMA, C.A</span><br>
+            <span class="text-xl">
+                Comercializadora de <span class="popBold">Insumos</span> y <span class="popBold">Agro-insumos</span>.
+            </span>
+            </p>
+        </div>
+        <button id="buttonSliderPrev" class="z-20 text-5xl text-white z-10 left-4 absolute flex items-center hover:text-yellow buttonSlider"> <i class="fi fi-sr-angle-left"></i> </button>
+        <button id="buttonSliderNext" class="z-20 text-5xl text-white z-10 right-4 absolute flex items-center hover:text-yellow buttonSlider"> <i class="fi fi-sr-angle-right"></i> </button>
         <div class="z-10 bottom-3 absolute flex items-center gap-4">
             <button class="text-md text-white flex items-center hover:text-yellow myButtonSlider"> <i class="fi fi-sr-circle-small"></i> </button>
             <button class="text-md text-white flex items-center hover:text-yellow myButtonSlider"> <i class="fi fi-sr-circle-small"></i> </button>
@@ -45,12 +56,12 @@
     <section class="w-full bg-white py-12 cut-top">
         <article class="text-white relative max-w-5xl rounded-md mx-auto w-full p-4 xl:p-8">
             <div class="flex flex-col justify-center items-center">
-                <picture class="logo-second">
+                <!-- <picture class="logo-second">
                     <img src="./src/logo-verde.png" alt="" srcset="">
-                </picture>
-                <p class="mt-4 pt-8 text-black text-center text-xl lg:text-3xl tracking-wider uppercase">
+                </picture> -->
+                <!-- <p class="mt-4 pt-8 text-black text-center text-xl lg:text-3xl tracking-wider uppercase">
                     Comercializadora de <br> <span class="popBold">Insumos</span> y <span class="popBold">Agro-insumos</span>.
-                </p>
+                </p> -->
             </div>
             <!-- <h2>Lorem ipsum dolor sit amet consectetur.</h2> -->
             <!-- <p>
@@ -215,6 +226,6 @@
     </div>
     </footer>
 </body>
-<script src="./js/app.js?" type="module"></script>
 
+<script src="./js/app.js?<?php echo $date ?>" type="module"></script>
 </html>
